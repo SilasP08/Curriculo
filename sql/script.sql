83f89d0d979f7e31a9a38c68cac14f5c5a294c09
SilasP0 <possarle.silas16@gmail.com>
2026-07-24 16:42:44 -0300

    Add SQL script for database schema and initial data insertion

---
+++ sql/script.sql
@@ -0,0 +1,39 @@
+use db_curri;
+
+Create table dados_pessoais (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    cargo VARCHAR(100) NULL,
+    informacao_principal VARCHAR(1000) NULL
+);
+
+Create table contato (
+    email VARCHAR(100) NOT NULL,
+    telefone VARCHAR(14) NOT NULL,
+    perfis VARCHAR(1000) NULL
+);
+
+Create table experiencias (
+    empresa VARCHAR(100) NULL,
+    funcao VARCHAR(100) NULL,
+    periodo INT NULL,
+    descricao VARCHAR(1000) NULL
+);
+
+Create table formacao (
+    instituicao VARCHAR(100) NULL,
+    curso VARCHAR(100) NULL,
+    periodo INT NULL
+);
+
+INSERT INTO dados_pessoais (
+    nome
+)VALUES (
+    "Silas Possarle"
+);
+
+INSERT INTO contato (
+    email, telefone
+)VALUES (
+    "silas30@gmail.com", "(11) 99999-1111"
+);
