9326e45868aacd60b2d7f0a2d4b601c8604e53f7
SilasP0 <possarle.silas16@gmail.com>
2026-07-31 13:12:25 -0300

    Começo do banner

--- index.php
+++ index.php
@@ -5,10 +5,45 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Curriculo</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
+    <main class="profile">
 
+        <section class="banner">
+
+        </section>
+
+        <section class="profile-card">
+
+            <div class="profile-image">
+
+                <img src="img/user.png" alt="Foto">
+
+            </div>
+
+            <div class="profile-info">
+
+                <h1>Nome do Usuário</h1>
+
+                <span>Desenvolvedor Full Stack</span>
+
+                <p>Santo André • SP</p>
+
+                <div class="profile-buttons">
+
+                    <button>Editar Perfil</button>
+
+                    <button>Compartilhar</button>
+
+                </div>
+
+            </div>
+
+        </section>
+
+    </main>
 </body>
 
 </html>
